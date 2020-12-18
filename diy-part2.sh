1586425f0fa9819cdc9f9ee8cc36b3923f96bc25
kenvint <34778826+kenvint@users.noreply.github.com>
2020-12-18 17:16:06 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,6 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+#删除Lean/packages下的luci-theme-argon
+rm -rf ./lede/package/lean/luci-theme-argon
