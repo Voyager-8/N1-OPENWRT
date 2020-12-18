3f85d878f146e08d87da61d8d7a7e54ffdcbfc5c
kenvint <34778826+kenvint@users.noreply.github.com>
2020-12-19 03:47:57 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,9 +9,13 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
-
+#
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-
+#
 #删除Lean/packages下的luci-theme-argon
 rm -rf ./package/lean/luci-theme-argon
+#
+#运行 ./scripts/feeds install -a 时候报错形如“WARNING: Makefile 'package/lean/amule/Makefile' has a dependency on 'libpng', which does not exist”
+#重新./scripts/feeds install -a
+./scripts/feeds install -a
