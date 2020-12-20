1164781b9ef30b5ed7af85919e4fd9630586548f
kenvint <34778826+kenvint@users.noreply.github.com>
2020-12-20 08:48:36 +0800

    Update diy-part2.sh
    修改luci-app-cpufreq的makefile的文件内容，适配ARM64
    #readd cpufreq for aarch64
    sed -i 's/LUCI_DEPENDS.*/LUCI_DEPENDS:=\@\(arm\|\|aarch64\)/g' package/lean/luci-app-cpufreq/Makefile
    修改默认IP 为10.10.10.50

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,18 +11,20 @@
 #
 #
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.10.10.50/g' package/base-files/files/bin/config_generate
 #
 #运行 ./scripts/feeds install -a 时候报错形如“WARNING: Makefile 'package/lean/amule/Makefile' has a dependency on 'libpng', which does not exist”
 #重新./scripts/feeds install -a
-#rm -rf ./feeds
+./scripts/feeds clean
 ./scripts/feeds update -a
-#./scripts/feeds install -a
+./scripts/feeds install -a
 #
 #删除Lean/packages/lean下的luci-theme-argon
-cd ./package/lean 
+#cd ./package/lean 
 rm -rf ./package/lean/luci-theme-argon
 #添加luci_theme_argon主题
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
 ##返回上两级目录(openwrt,lean目录)
-cd ../..
+#cd ../..
+#readd cpufreq for aarch64
+sed -i 's/LUCI_DEPENDS.*/LUCI_DEPENDS:=\@\(arm\|\|aarch64\)/g' package/lean/luci-app-cpufreq/Makefile
