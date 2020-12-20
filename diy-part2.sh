05c5972215d48bd5c01caa9c9654808ae705dc04
kenvint <34778826+kenvint@users.noreply.github.com>
2020-12-20 11:23:16 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,14 +16,16 @@ sed -i 's/192.168.1.1/10.10.10.50/g' package/base-files/files/bin/config_generat
 #运行 ./scripts/feeds install -a 时候报错形如“WARNING: Makefile 'package/lean/amule/Makefile' has a dependency on 'libpng', which does not exist”
 #重新./scripts/feeds install -a
 ./scripts/feeds clean
+#添加luci_theme_argon主题
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 #
 #删除Lean/packages/lean下的luci-theme-argon
 #cd ./package/lean 
-rm -rf ./package/lean/luci-theme-argon
+#rm -rf ./package/lean/luci-theme-argon
 #添加luci_theme_argon主题
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
+#git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
 #./scripts/feeds update infinityfreedom
 #./scripts/feeds install luci-theme-infinityfreedom
 ##返回上两级目录(openwrt,lean目录)
