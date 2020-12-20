6b896ec3c1f026d2cfde317bc1deaa61a22772d8
kenvint <34778826+kenvint@users.noreply.github.com>
2020-12-20 20:33:52 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,9 +14,11 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
 #删除lean内的luci-theme-argon
+cd ./package/lean  
+rm -rf luci-theme-argon 
 #rm -rf ./package/lean/luci-theme-argon
 
 #添加luci_theme_argon主题
